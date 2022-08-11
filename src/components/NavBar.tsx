39d1474e3c6c51997d412654361e5a84f0bcc48b
Gabriel <gabrielmoon777@gmail.com>
2022-08-11 18:59:18 +0100

    Fixed navbar position

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -9,7 +9,7 @@ export const NavBar = () => {
     <div className=" flex h-20 w-full items-center justify-center bg-[#3D7DCA] shadow-md shadow-black">
 
 
-      <div className="flex h-full">
+      <div className="flex h-full items-center">
         <img className="mr-2 h-[80%]" src={Charmander} alt="Charmander" />
         <img className="h-[80%]" src={Logo} alt="logo" />
         <img className="ml-2 h-[80%]" src={Squirtle} alt="Squirtle" />
